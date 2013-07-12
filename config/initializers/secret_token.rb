a9a2e67e98e5deffcb7bcf206c55cf1b5c262982
Subrat Rout <subratnrout@gmail.com>
2013-07-11 20:26:37 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medicaldevicehub::Application.config.secret_key_base = '44c5e40b993b05415d7d08e100863393fed9d148c3954c1ea5bc98d01a6fee026b62a19e9cf01ee742418c049d1c2eaab0075ecc4634d28e9d4bdd2dc79a3d1e'
