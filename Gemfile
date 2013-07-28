19a0ceb309ac80c99cb469ca11e57cd474cec9a6
Subrat Rout <subratnrout@gmail.com>
2013-07-28 13:13:22 -0700

    Added test spec and modified medicaldevice.rb file

--- Gemfile
+++ Gemfile
@@ -50,9 +50,12 @@ gem "email_spec", ">= 1.4.0", :group => :test
 gem "cucumber-rails", :group => :test, :require => false
 gem "capybara", :group => :test
 gem "factory_girl_rails", ">= 4.2.0", :group => [:development, :test]
+gem "guard-rspec"
 gem "simple_form", "3.0.0.rc"
 gem "better_errors", ">= 0.7.2", :group => :development
 gem "paperclip", "~>3.0"
+gem 'aws-sdk'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/medicaldevice.rb
+++ app/models/medicaldevice.rb
@@ -6,5 +6,5 @@ class Medicaldevice < ActiveRecord::Base
   validates_attachment :image, presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
-  has_attached_file :image, styles: { medium: "320x240>"}
+  has_attached_file :image, styles: { medium: "320x240>", :thumb => "100x100>"}, :default_url => "/images/:style/missing.png"
 end
