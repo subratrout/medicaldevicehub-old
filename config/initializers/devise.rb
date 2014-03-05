20efc586e20eb3fef0e7333d090aa782d65139e9
Subrat Rout <subratnrout@gmail.com>
2014-03-04 20:43:41 -0800

    Added a temp soln to devise tokenauth deprecation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
-
+  config.secret_key = 'mdhub1234devisesecretkey' if Rails.env == 'production'
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
